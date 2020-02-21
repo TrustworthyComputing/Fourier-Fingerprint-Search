ec394353df2f91fe6593f5fbd2572b2c8ccba73d
Dimitris Mouris <jimouris@udel.edu>
2020-02-21 11:27:39 -0500

    Add STL-to-array, scaling, initializing the grid and slicing.

--- helper.py
+++ helper.py
@@ -1,10 +1,24 @@
+import os
+import argparse
 from enum import Enum
 
 class Axis(Enum):
     X = 1
     Y = 2
     Z = 3
+    
+class Point:
+    def __init__(self, token):
+        self.x = float(token[1])
+        self.y = float(token[2])
+        self.z = float(token[3])
 
+    def print_point(self):
+        print(str(self.x) + ' \t ' + str(self.y) + ' \t ' + str(self.z))
+
+'''
+Parse arguments and perform checks.
+'''
 def parseArgs():
     parser = argparse.ArgumentParser(description='STL compression')
     parser.add_argument('--stl', help='path to STL file (.stl)', required=True)
@@ -21,12 +35,40 @@ def parseArgs():
     if args.slices is not None:
         slices = args.slices
     return stl_input, int(slices)
-    
-class Point:
-    def __init__(self, x, y, z):
-        self.x = x
-        self.y = y
-        self.z = z
 
-    def print_point(self):
-        print(str(self.x) + ' \t ' + str(self.y) + ' \t ' + str(self.z))
+'''
+Sort the list for the given axis
+'''
+def sort_by_axis(axis, points):
+    if axis == Axis.X:
+        return sorted(points, key=lambda point: point.x)
+    elif axis == Axis.Y:
+        return sorted(points, key=lambda point: point.y)
+    elif axis == Axis.Z:
+        return sorted(points, key=lambda point: point.z)
+    else:
+        print("No such axis.")
+
+'''
+Find min, max and range for the given axis
+'''
+def find_max_min_range(points_array, axis):
+    if axis == Axis.X:
+        max_x = max(p.x for p in points_array)
+        min_x = min(p.x for p in points_array)
+        range_x = max_x - min_x
+        return max_x, min_x, range_x
+    elif axis == Axis.Y:
+        max_y = max(p.y for p in points_array)
+        min_y = min(p.y for p in points_array)
+        range_y = max_y - min_y
+        return max_y, min_y, range_y
+    elif axis == Axis.Z:
+        max_z = max(p.z for p in points_array)
+        min_z = min(p.z for p in points_array)
+        range_z = max_z - min_z
+        return max_z, min_z, range_z
+
+def quantizer(num, accuracy=0.02):
+    factor = 1.0 / accuracy
+    return round(num*factor)/factor
---
+++ main.py
@@ -0,0 +1,115 @@
+import sys
+import pyfftw
+import math
+import numpy as np
+from helper import *
+from operator import itemgetter
+from scipy.ndimage.filters import maximum_filter
+from scipy.ndimage.morphology import (generate_binary_structure, iterate_structure, binary_erosion)
+
+'''
+The size of the grid that all shapes will scale to.
+'''
+GRID_SIZE = 1000
+
+'''
+Degree to which a fingerprint can be paired with its neighbors -- higher will cause more fingerprints, but potentially better accuracy.
+'''
+DEFAULT_FAN_VALUE = 15
+
+'''
+If True, will sort peaks temporally for fingerprinting; not sorting will cut down number of fingerprints, but potentially affect performance.
+'''
+PEAK_SORT = True
+
+
+'''
+* Open fileName STL file
+* Parse the points
+* Return array of 3D points  
+'''
+def stl_to_points_array(fileName):
+    stl = open(fileName, "r")
+    points_array = []
+    triangle = []
+    points_count = 0
+    in_triangle = False
+    for line in stl:
+        if "vertex" in line and in_triangle:
+            points_count +=1
+            tokens = line.split()
+            p = Point(tokens)
+            triangle.append(p)
+        elif "outer loop" in line:
+            in_triangle = True
+        elif "endloop" in line:
+            in_triangle = False
+            assert(points_count == 3)
+            points_count = 0
+# TODO: interpolate
+            for p in triangle:
+                points_array.append(p)
+            triangle = []
+    stl.close()
+    return points_array
+
+'''
+Determine scale factor and update all points. 
+'''
+def scale_points(points_array, grid_size=1000):
+    # Find scale factor
+    max_x, min_x, range_x = find_max_min_range(points_array, Axis.X)
+    max_y, min_y, range_y = find_max_min_range(points_array, Axis.Y)
+    max_z, min_z, range_z = find_max_min_range(points_array, Axis.Z)
+    scale_factor = float(grid_size-1) / max(range_x, range_y, range_z)
+    # Update points
+    for i in range(len(points_array)):
+        points_array[i].x = int(scale_factor * (points_array[i].x - min_x) )
+        points_array[i].y = int(scale_factor * (points_array[i].y - min_y) )
+        points_array[i].z = int(scale_factor * (points_array[i].z - min_z) )
+    return points_array
+
+
+'''
+Generate the fingerprint of a STL file and store it in the hash table.
+    stl_file        : STL input file name.
+    num_of_slices   : Number of slices to split the STL file to.
+'''
+def fingerprint(stl_file, num_of_slices):
+    # Parse STL and interpolate points
+    points_array = stl_to_points_array(stl_file)
+    
+    # Scale points
+    scaled_points_array = scale_points(points_array)
+
+    # Sort by Z axis
+    scaled_points_array = sort_by_axis(Axis.Z, scaled_points_array)
+    
+    # For each slice 
+    points_per_slice = math.ceil(len(scaled_points_array)/num_of_slices)
+    for i in range(num_of_slices):
+        
+        # Put points on the grid 
+        grid = np.zeros((GRID_SIZE, GRID_SIZE))
+        for j in range(points_per_slice):
+            
+            idx = (i*points_per_slice) + j
+            if (idx >= len(scaled_points_array)):
+                break
+            p = scaled_points_array[idx]
+            grid[p.x][p.y] = 1
+        
+        # FTT
+
+
+
+'''
+Driver function
+'''
+def main():
+    stl_file, num_of_slices = parseArgs()
+    fingerprint(stl_file, num_of_slices)
+    
+
+if __name__ == "__main__":
+    main()
