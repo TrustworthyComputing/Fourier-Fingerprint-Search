b11ba2f62f905a44b153e810e6a5a0399473f85d
Dimitris Mouris <jimouris@udel.edu>
2020-05-05 12:30:57 -0400

    Fix print message

--- benchmarks/benchmark.sh
+++ benchmarks/benchmark.sh
@@ -4,7 +4,7 @@ DATASET_PATH=benchmarks/FabWave
 MODE="$1"
 SLICES=2
 FANOUT=10
-MINSIGS=10
+MINSIGS=6
 
 if [ "$MODE" = "learn" ]; then
     echo "Destroying the database"
@@ -16,10 +16,10 @@ for d in $DATASET_PATH/*/ ; do
     filename=$(basename -- "$d")
 
     if [ "$MODE" = "learn" ]; then
-        echo "Learn $filename slices $SLICES fanout $FANOUT"
+        echo "Learn $filename slices:$SLICES fanout:$FANOUT"
         (time python3 main.py --stl "$d" --mode learn --slices $SLICES --fanout $FANOUT ) &> results/learn_"$filename"_s"$SLICES"_f"$FANOUT".txt
     else
-        echo "Search $filename slices $SLICES fanout $FANOUT"
+        echo "Search $filename slices:$SLICES fan-out:$FANOUT minimum-sigs-in-neighborhood:$MINSIGS"
         (time python3 main.py --stl "$d" --mode search --slices $SLICES --fanout $FANOUT --print_naive --neighborhoods --sigs_in_neighborhood $MINSIGS) &> results/search_"$filename"_s"$SLICES"_f"$FANOUT"_min"$MINSIGS".txt
     fi
 
