5cb0e23a5b98435c3dd999021354ee8872d67bda
Dimitris Mouris <jimouris@udel.edu>
2020-05-05 12:30:26 -0400

    Add classification checker

---
+++ benchmarks/checker_common.py
@@ -0,0 +1,50 @@
+from enum import Enum
+
+DEBUG = False
+PRINT_LEN = 64
+
+
+class Mode(Enum):
+    Naive = 1
+    Neighborhoods = 2
+
+
+def log(str):
+    if DEBUG:
+        print(str)
+
+
+def line_empty(line):
+    return len(line.split('\t')) < 3
+
+
+def get_query_filename_from_line(line):
+    query = ''
+    prevWord = ''
+    for word in line.split(' '):
+        if 'with' in prevWord:
+            query = word
+            break
+        prevWord = word
+    return query.split('/')[-1].strip('\n')
+
+
+def get_query_class_from_line(line):
+    query = ''
+    prevWord = ''
+    for word in line.split(' '):
+        if 'with' in prevWord:
+            query = word
+            break
+        prevWord = word
+    return query.split('/')[-2].strip('\n')
+
+
+def get_answer_filename_from_line(line):
+    filename = line.split('\t')[-3]
+    return filename.split('/')[-1].strip('\n')
+
+
+def get_answer_class_from_line(line):
+    filename = line.split('\t')[-3]
+    return filename.split('/')[-2].strip('\n')
