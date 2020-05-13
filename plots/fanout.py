82730f0322d962852db16286591e0103f993943a
Dimitris Mouris <jimouris@udel.edu>
2020-05-13 15:24:23 -0400

    Add fanout experiments for classification

--- plots/fanout.py
+++ plots/fanout.py
@@ -6,9 +6,10 @@ mydpi = 300
 
 pltsize = (7, 3)
 
-naive_accuracy = [.506, .511, .521, .545, .616, .672 ]
-neighborhoods_accuracy = [.524, .546, .584, .643, .746, .835 ]
-Time = [ 39.11, 48.99, 70.05, 100.09, 136.34, 151.28]
+# TOP 3 accuracy
+fine_grained_accuracy = [ 0.893, 0.947, 0.893, 0.92, 0.916 ]
+neighborhoods_accuracy = [ 0.83, 0.867, 0.813, 0.84, 0.882 ]
+Time = [ 1.11, 1.15, 1.15, 1.18, 1.21]
 
 N = len(neighborhoods_accuracy)
 index = np.arange(N)  # the x locations for the groups
@@ -18,16 +19,16 @@ ax2 = ax.twinx()  # instantiate a second axes that shares the same x-axis
 
 ax.set_xticks(index)
 ax.set_xlabel('Fan-out value', fontsize=13)
-ax.set_xticklabels(['$5$', '$10$', '$20$', '$40$', '$80$', '$160$'], fontsize=11)
+ax.set_xticklabels(['$5$', '$10$', '$15$', '$20$', '$25$'], fontsize=11)
 
 ax.tick_params(axis='both', which='major', labelsize=11)
 ax.tick_params(axis='both', which='minor', labelsize=11)
 
-l1 = ax.plot(index, naive_accuracy, linestyle='solid', color='black', markerfacecolor='#076794', marker='^', linewidth=1, markersize=8)
+l1 = ax.plot(index, fine_grained_accuracy, linestyle='solid', color='black', markerfacecolor='#076794', marker='^', linewidth=1, markersize=8)
 l2 = ax.plot(index, neighborhoods_accuracy, linestyle='solid', color='black', markerfacecolor='#14b4ff', marker='D', linewidth=1, markersize=8)
 
 ax.set_ylabel('Accuracy', fontsize=13)
-ax.set_ylim([0.5, 0.9])
+ax.set_ylim([0.8, 1])
 
 l3 = ax2.plot(index, Time, linestyle='solid', color='black', markerfacecolor='xkcd:grey', marker='s', linewidth=1, markersize=8)
 ax2.set_ylabel('Time (sec.)', fontsize=13)
@@ -35,10 +36,11 @@ ax2.set_ylabel('Time (sec.)', fontsize=13)
 
 ax2.tick_params(axis='both', which='major', labelsize=11)
 ax2.tick_params(axis='both', which='minor', labelsize=11)
-ax2.set_ylim([25, 200])
+# ax2.set_ylim([25, 200])
 
 
-ax.legend((l1[0], l2[0], l3[0]), ['Naive', 'Neighborhoods', 'Time'], fontsize=11, ncol=1, loc='upper left')
+ax.legend((l1[0], l2[0], l3[0]), ['Fine-grained', 'Neighborhoods', 'Average time per file'], fontsize=11, ncol=3, bbox_to_anchor=(0, 1.02, 1, 0.2), loc='lower center')
+
 
 
 plt.tight_layout()
